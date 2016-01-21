bbdd3f84b8d3a97f37d688144ac0a4c59251252d
Derek Kniffin <derek.kniffin@gmail.com>
2016-01-21 08:25:47 -0500

    Move the main view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def angular
-    render 'main'
+  def main
+    render 'game/map'
   end
 end
