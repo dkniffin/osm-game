25415d6f1b24fe5751adcad28942ca0dd0378992
Derek Kniffin <derek.kniffin@gmail.com>
2016-03-15 12:28:41 -0400

    Remove unused method

--- app/models/osm/way.rb
+++ app/models/osm/way.rb
@@ -6,11 +6,5 @@ module OSM
     self.primary_key = 'osm_id'
 
     geometry_attribute :way
-
-    def nodes
-      read_attribute(:nodes).map do |node_id|
-        Node.find(node_id)
-      end
-    end
   end
 end
