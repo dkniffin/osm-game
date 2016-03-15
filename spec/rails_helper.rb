03b5d5e152576432aab9ad517fecea04f6a80012
Patrick Rawlings <patrick.rawlings@smashingboxes.com>
2016-03-15 14:52:43 -0400

    Test; using transactional fixtures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,5 +9,6 @@ Dir[Rails.root.join("spec/models/concerns/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
 end
