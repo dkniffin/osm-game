050a335ef51cef4b1bc756c99f657489e96c2985
Derek Kniffin <derek.kniffin@gmail.com>
2016-04-08 18:37:59 -0400

    Added a test for Item.equipped

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,6 +6,8 @@ describe Item, type: :model do
   it { is_expected.to respond_to(:name) }
   it { is_expected.to respond_to(:category) }
   it { is_expected.to respond_to(:stats) }
+  it { is_expected.to respond_to(:equippable?) }
+  it { is_expected.to respond_to(:currently_equipped?) }
   it { is_expected.to belong_to(:character) }
 
   describe '.weapon' do
@@ -15,6 +17,16 @@ describe Item, type: :model do
 
     it 'returns items with a category of weapon' do
       expect(subject).to eq(weapons)
+    end
+  end
+
+  describe '.equipped' do
+    subject { Item.equipped }
+    let!(:equipped) { create_list(:item, 3, currently_equipped: true) }
+    let!(:not_equipped) { create_list(:item, 3, currently_equipped: false) }
+
+    it 'returns items that are currently equipped' do
+      expect(subject).to eq(equipped)
     end
   end
 end
