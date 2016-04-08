3f86e5d8b32d54a3d76a8e0c22f8e85a625bc489
Derek Kniffin <derek.kniffin@gmail.com>
2016-04-08 18:35:52 -0400

    Added a test for Item.weapon

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,4 +7,14 @@ describe Item, type: :model do
   it { is_expected.to respond_to(:category) }
   it { is_expected.to respond_to(:stats) }
   it { is_expected.to belong_to(:character) }
+
+  describe '.weapon' do
+    subject { Item.weapon }
+    let!(:weapons) { create_list(:item, 3, category: 'weapon') }
+    let!(:non_weapons) { create_list(:item, 3, category: 'non_weapon') }
+
+    it 'returns items with a category of weapon' do
+      expect(subject).to eq(weapons)
+    end
+  end
 end
