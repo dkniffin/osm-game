774266bf7c25cb456b8dc1dd92d156bfb871541c
Derek Kniffin <derek.kniffin@gmail.com>
2016-04-28 09:49:24 -0400

    Got basic facebook oauth set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: 'application#main'
   get '/3d', to: 'application#three_d'
   get '/2d', to: redirect('/')
